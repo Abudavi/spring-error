5e73166cb95c7b82e54718c53be201b0d8318fbc
David MTZD <davidmtzd21512@gmail.com>
2024-09-25 17:46:24 -0600

    se creo la implementacion para buscar los datos de los users

---
+++ src/main/java/com/david/spring/error/spring_error/services/UserServiceImp.java
@@ -0,0 +1,41 @@
+package com.david.spring.error.spring_error.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.david.spring.error.spring_error.models.domain.User;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    private List<User> users;
+
+    public UserServiceImp() {
+        this.users = new ArrayList<>();
+        users.add(new User(1L, "Pepe", "Gonzales"));
+        users.add(new User(2L, "Andres", "Mena"));
+        users.add(new User(3L, "Maria", "Perez"));
+        users.add(new User(4L, "Jose", "Gonzales"));
+        users.add(new User(5L, "Ale", "Guitierrez"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return users;
+    }
+
+    @Override
+    public User findById(Long id) {
+        User u = null;
+        for (User user : users) {
+            if (user.getId().equals(id)) {
+                u = user;
+                break;
+            }
+        }
+        return u;
+    }
+
+}
