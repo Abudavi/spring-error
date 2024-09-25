2e91f301123b240b81640ef86d71147ffb12cf4c
David MTZD <davidmtzd21512@gmail.com>
2024-09-25 17:06:49 -0600

    se agrego el manejo de errores para las paginas no encontadas

--- src/main/java/com/david/spring/error/spring_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/david/spring/error/spring_error/controllers/HandlerExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.david.spring.error.spring_error.models.Error;
 
@@ -21,6 +22,21 @@ public class HandlerExceptionController {
 
         // return ResponseEntity.internalServerError().body(error);//esta o la de abajo
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+
+    }
+
+    // aqui se soluciona de dos formas poner NoHandlerFoundException y agregar
+    // configuraciones en el properties
+    // o sustituir NoHandlerFoundException por NoResourceFoundException
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFondException(NoHandlerFoundException e) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Api rest no encontrado");
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(404).body(error);
 
     }
 
