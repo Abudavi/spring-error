105b4cd3f70d92782afca02d76ca1623f106c867
David MTZD <davidmtzd21512@gmail.com>
2024-09-25 16:48:25 -0600

    se agrego una forma mas detallada de manejar el error de ArithmeticException

--- src/main/java/com/david/spring/error/spring_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/david/spring/error/spring_error/controllers/HandlerExceptionController.java
@@ -1,5 +1,8 @@
 package com.david.spring.error.spring_error.controllers;
 
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,9 +12,16 @@ import com.david.spring.error.spring_error.models.Error;
 @RestControllerAdvice
 public class HandlerExceptionController {
     @ExceptionHandler({ ArithmeticException.class })
-    public ResponseEntity<?> divisionByZero(Exception ex) {
+    public ResponseEntity<Error> divisionByZero(Exception ex) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Error Division entre 0!");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 
-        return ResponseEntity.internalServerError().body("error 500");
+        // return ResponseEntity.internalServerError().body(error);//esta o la de abajo
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+
     }
 
 }
