afbabd218ac929292809fadf29d3a469b745d1a4
David MTZD <davidmtzd21512@gmail.com>
2024-09-25 16:24:42 -0600

    creacion del proyecto

---
+++ src/main/java/com/david/spring/error/spring_error/SpringErrorApplication.java
@@ -0,0 +1,13 @@
+package com.david.spring.error.spring_error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringErrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/spring/error/spring_error/SpringErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.spring.error.spring_error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
